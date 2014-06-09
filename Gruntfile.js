3f3fd9e794f01b6566e09521b621ed19d1fea6d1
joseX2R <jlrepresa@gmail.com>
2014-06-09 17:31:00 +0200

    Forgot add grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks("grunt-coveralls");
+  grunt.loadNpmTasks("grunt-qunit-istanbul");
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
